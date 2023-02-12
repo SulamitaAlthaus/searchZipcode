232ec57492cc7181a29bbe75c14e8cd012704a25
Sulamita Althaus <sulamitaalthaus44@gmail.com>
2023-02-12 11:43:52 -0300

    chore: add cors

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'zipcode'
 ]
 
@@ -49,7 +50,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5173',
+]
+
 
 ROOT_URLCONF = 'app.urls'
 
