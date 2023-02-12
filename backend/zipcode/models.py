c6b5db695501d3e8c8643c1a85896c9c4da4c168
Sulamita Althaus <sulamitaalthaus44@gmail.com>
2023-02-12 11:42:12 -0300

    refactor: adjust zipcode table field types

--- backend/zipcode/models.py
+++ backend/zipcode/models.py
@@ -8,10 +8,10 @@ class ZipCode(models.Model):
     bairro = models.CharField(max_length=256, null=True, blank=True)
     localidade = models.CharField(max_length=256, null=True, blank=True)
     uf = models.CharField(max_length=2, null=True, blank=True)
-    ibge = models.IntegerField(null=True, blank=True)
-    gia = models.IntegerField(null=True, blank=True)
-    ddd = models.IntegerField(null=True, blank=True)
-    siafi = models.IntegerField(null=True, blank=True)
+    ibge = models.CharField(max_length=100, null=True, blank=True)
+    gia = models.CharField(max_length=100, null=True, blank=True)
+    ddd = models.CharField(max_length=2, null=True, blank=True)
+    siafi = models.CharField(max_length=256, null=True, blank=True)
     lojacorr = models.BooleanField(default=False)
 
     def __str__(self):
