4fdb8c786672aa629be239969a1993380be3ccf7
Sulamita Althaus <sulamitaalthaus44@gmail.com>
2023-02-12 17:11:16 -0300

    refactor: create components SearchZipcode and CardAddress

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,122 +1,72 @@
 <template>
     <div class="container">
-        <div class="search">
-            <label for="">Digite o CEP: </label>
-            <input type="text" v-model="zipcode">
+        <div class="searchArea">
+            <SearchZipcode @address="getAddress" />
+            <CardAddress v-if="obj.address" :address="obj.address" @updateAddresses="getAddresses" />
         </div>
-        <button @click="searchZipcode(zipcode)">Search</button>
-        <div v-if="obj.address" class="contentContainer">
-            <div class="content">
-                <span>CEP: {{ obj.address.cep }}</span>
-                <span>Logradouro: {{ obj.address.logradouro }}</span>
-                <span>UF: {{ obj.address.uf }}</span>
-            </div>
-                <button @click="createZipcode(obj.address)">+</button>
-        </div>
-        <div class="table" v-if="obj.addresses">
-            <Table :addresses=obj.addresses @updateAddresses="getAddresses"/>
+        <div class="table" v-if="obj.openTable">
+            <Table :addresses=obj.addresses @updateAddresses="getAddresses" />
         </div>
     </div>
 </template>
 
 <script setup>
-    import { reactive, onMounted } from "vue"
-    import api from "../services/api"
-    import Table from "../components/Table.vue"
+import { reactive, onMounted } from "vue"
+import api from "../services/api"
+import Table from "../components/Table.vue"
+import SearchZipcode from "../components/SearchZipcode.vue"
+import CardAddress from "../components/CardAddress.vue"
 
-    let zipcode
+const obj = reactive({
+    address: '',
+    addresses: [],
+    openTable: false
+})
 
-    const obj = reactive({
-        address: '',
-        addresses: []
-    })
+onMounted(() => {
+    getAddresses()
+})
 
-    onMounted(() => {
-       getAddresses()
-    })
+async function getAddress(address) {
+    obj.address = address
+}
 
-    async function searchZipcode(zipcode) {
-        zipcode = zipcode.replace(/[^0-9]/g, '');
-        try {
-            const response = await fetch(`https://viacep.com.br/ws/${zipcode}/json/`)
-            obj.address = await response.json()
-        } catch (err) {
-            console.error(err)
-        }
+async function getAddresses() {
+    const response = await api.get("/cep");
+    obj.addresses = response.data;
+    if (obj.addresses.length > 0) {
+        obj.openTable = true
+    } else {
+        obj.openTable = false
     }
-    async function getAddresses() {
-        const response = await api.get("/cep");
-        console.log("response", response)
-        obj.addresses = response.data;
-    }
-
-    async function createZipcode(address) {
-        await api.post("/cep", address);
-        getAddresses()
-    }
+}
 
 </script>
 
 <style scoped>
-.container{
+.container {
     display: flex;
     flex-direction: column;
+    gap: 2rem;
 }
-.search{
-    padding: 8px 0;
-}
-.contentContainer{
+
+
+.searchArea {
     display: flex;
-}
-.contentContainer button{
-    width: 2rem;
-    height: 2rem;
-    margin: 1rem;
-    display: flex;
-    justify-content: center;
     align-items: center;
-}
-.content{
-    display: flex;
-    flex-direction: column;
-    padding: 16px 0;
-    text-align: left;
-}
-.table{
+    justify-content: start;
+    gap: 4rem;
+    height: 10vh;
+    margin: 2rem 0;
     width: 90vw;
-    margin-top: 4rem;
+}
+
+.table {
+    width: 90vw;
+    margin-top: 2rem;
     border: 1px solid gray;
+    border-radius: 0.8rem;
     text-align: left;
     justify-content: space-between;
-}
-.tableHeader{
-    font-weight: bold;
-    width: 100%;
-    height: 2rem;
-    display: flex;
-    justify-content: space-between;
-}
-#tableColumnTitle{
-    padding: 0 2rem;
-    width: 10%;
-    height: 2rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid gray;
-}
-.tableCell{
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: space-around;
-    border: 1px solid gray;
-    text-align: center;
-
-}
-#tableCellContent{
-    width: 10%;
-    padding: 0.2rem ;
-
 }
 </style>
